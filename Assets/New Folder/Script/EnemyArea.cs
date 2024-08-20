c3b13b37990d2411dbc138b108b2af240d80a68f
una1220 <mikiya122000@gmail.com>
2024-08-21 00:32:11 +0900

    test_1

---
+++ Assets/New Folder/Script/EnemyArea.cs
@@ -0,0 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArea : MonoBehaviour
+{
+    public GameObject[] EnemyList;
+
+    void Start()
+    {
+        foreach (var enemy in EnemyList)
+        {
+            enemy.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            foreach (var enemy in EnemyList)
+            {
+                enemy.SetActive(true);
+            }
+
+            //ˆê“x“G‚ğ—LŒø‰»‚µ‚½‚çA“–‚½‚ç‚È‚¢‚æ‚¤‚É‚·‚é
+            var collider = GetComponent<Collider>();
+            collider.enabled = false;
+        }
+    }
+}
